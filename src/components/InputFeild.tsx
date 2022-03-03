21c4862ab33d96edd008cd3e833199aa24b779bc
kiran.karki <kiran.karki@student.fh-kiel.de>
2022-03-03 09:37:39 +0100

    adding task into task array state

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './styles.css'
 
-const InputFeild = () => {
+interface Props{
+	todo:string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd:(e:React.FormEvent)=>void;
+}
+// const InputFeild = ({todo,setTodo}:Props) => {
+	const InputFeild:React.FC<Props>= ({todo,setTodo,handleAdd}) => {
   return (
 	<div>
-	  <form className='input'>
-		  <input type="input" placeholder='Enter a task' className='input__box' />
+	  <form className='input' onSubmit={handleAdd} >
+		  <input type="input" placeholder='Enter a task' className='input__box'
+		   value={todo}
+			onChange={(e)=>setTodo(e.target.value)} />
 		  <button className='input_submit' type='submit'>Go</button>
 	</form>
 	</div>
